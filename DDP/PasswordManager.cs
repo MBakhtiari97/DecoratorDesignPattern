f98394502c01409125221bd4d3ad2a3a7ad4417c
Mohammad Bakhtiari <mohammadba86@yahoo.com>
2024-05-14 18:19:08 +0330

    Add project files.

---
+++ DDP/PasswordManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+namespace DecoratorDesignPattern.DDP;
+public static class PasswordManager
+{
+    public static string HashUserPassword(string password)
+    {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < bytes.Length / 4; i++)
+                builder.Append(bytes[i].ToString("x4"));
+            return builder.ToString();
+        }
+    }
+}
---
+++ DDP/Program.cs
@@ -0,0 +1,44 @@
+﻿using DecoratorDesignPattern.DDP;
+
+//Step 1 : Main interface  
+public interface IComponentPasswordReader
+{
+    string Read();
+}
+
+
+//Step 2 : Interface implementation
+public class ComponentPasswordReader : IComponentPasswordReader
+{
+    public string Read()
+    {
+        return "This is a secure password !";
+    }
+}
+
+
+//Step 3 : We need to have the date in persian calendar so we create a new component for that
+public class ComponentHashedPasswordReaderDecorator : IComponentPasswordReader
+{
+    private readonly IComponentPasswordReader _componentPasswordReader;
+    public ComponentHashedPasswordReaderDecorator(IComponentPasswordReader componentPasswordReader)
+    {
+        _componentPasswordReader = componentPasswordReader;
+    }
+    public string Read()
+    {
+        return PasswordManager.HashUserPassword(_componentPasswordReader.Read());
+    }
+}
+
+
+//Step 4 : Check output result
+public class CheckResult
+{
+    static void Main(string[] args)
+    {
+        IComponentPasswordReader componentPasswordReader = new ComponentPasswordReader();
+        IComponentPasswordReader componentHashedPasswordReaderDecorator = new ComponentHashedPasswordReaderDecorator(componentPasswordReader);
+        Console.WriteLine(componentHashedPasswordReaderDecorator.Read());
+    }
+}
